4e83628ff093e325e6e948cd485e224ead3e8a12
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-04-24 15:33:14 -0400

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opplx',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
